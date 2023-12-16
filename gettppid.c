99c3fa065f8eb5b10e1d55fa442807e486ce6c3c
fatima9821 <bahfatoumat9821@gmail.com>
2023-12-16 04:02:34 -0800

    gettppid.c

--- gettppid.c
+++ gettppid.c
@@ -6,6 +6,6 @@ int main(void)
 	pid_t my_pid;
 
 	my_pid = getppid();
-	printf("pid du processus parend : %u\n", my_pid)
-		return (0);
+	printf("pid du processus parend : %u\n", my_pid);
+	return (0);
 }
