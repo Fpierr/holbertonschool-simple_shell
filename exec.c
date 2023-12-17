65811bfe99f3486c8d78f2f8f4978bdcb8865a90
root <fanuelp9@gmail.com>
2023-12-17 14:18:33 -0800

    simple shell file creating

---
+++ exec.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * execute_command - fonction to execute a command in the simple shell
+ * @cmd: the command to execute
+ *
+ * Return: int
+ */
+int execute_command(command_t *cmd)
+{
+	pid_t pid;
+
+	if (cmd == NULL || cmd->args == NULL || cmd->args[0] == NULL)
+	{
+		fprintf(stderr, "Error: Invalid command.\n");
+		return (-1);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		execute_child_process(cmd);
+	}
+	else
+	{
+		execute_parent_process(cmd);
+	}
+	return (0);
+}
+
+/**
+ * execute_child_process - function tha execute the child process
+ * @cmd: the command chile process to execute
+ *
+ * Return: void
+ */
+void execute_child_process(command_t *cmd)
+{
+	int fd;
+
+	if (cmd->input_redirect != NULL)
+	{
+		fd = open(cmd->input_redirect, O_RDONLY);
+		if (fd == -1)
+		{
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
+	if (cmd->output_redirect != NULL)
+	{
+		fd = open(cmd->output_redirect, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (fd == -1)
+		{
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+	if (execvp(cmd->args[0], cmd->args) == -1)
+	{
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * execute_parent_process - fonction to execute the parent process of a command
+ * @cmd: the command to process execute
+ * @child_pid: the process ID of the child process
+ *
+ * Return: void
+ */
+void execute_parent_process(command_t *cmd, pid_t child_pid)
+{
+	int status;
+
+	if (!cmd->background)
+	{
+		waitpid(child_pid, &status, 0);
+	}
+}
---
+++ lexer.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+ * get_next_symbol - a lexer function that identificate the symbols type
+ * @input: the symbol to input in the terminal
+ *
+ * Return: the symbol type
+ */
+symbol_t *get_next_symbol(char *input)
+{
+	symbol_t *symbol;
+	size_t len;
+	char *start;
+
+
+	if (input == NULL || *input == '\0')
+		return (NULL);
+
+	symbol = malloc(sizeof(symbol_t));
+	symbol->value = NULL;
+
+	while (*input == ' ' || *input == '\t')
+		input++;
+
+	/* Identifiying symbol */
+	if (*input == '\0')
+	{
+		symbol->type = SYMBOL_EOL;
+	}
+	else if (*input == '|')
+	{
+		symbol->type = SYMBOL_PIPE;
+		symbol->value = strdup("|");
+		input++;
+	}
+	else if (*input == '<')
+	{
+		symbol->type = SYMBOL_REDIRECT_IN;
+		symbol->value = strdup("<");
+		input++;
+	}
+	else if (*input == '>')
+	{
+		symbol->type = SYMBOL_REDIRECT_OUT;
+		symbol->value = strdup(">");
+		input++;
+	}
+	else if (*input == '&')
+	{
+		symbol->type = SYMBOL_BACKGROUND;
+		symbol->value = strdup("&");
+		input++;
+	}
+	else if (*input == ';')
+	{
+		symbol->type = SYMBOL_SEMICOLON;
+		symbol->value = strdup(";");
+		input++;
+	}
+	else
+	{
+		symbol->type = SYMBOL_WORD;
+		start = input;
+
+		while (*input != ' ' && *input != '\t' && *input != '\0' &&
+				*input != '|' && *input != '<' && *input != '>' &&
+				*input != '&' && *input != ';')
+		{
+			input++;
+		}
+		len = input - start;
+		symbol->value = malloc(len + 1);
+		strncpy(symbol->value, start, len);
+		symbol->value[len] = '\0';
+	}
+
+	return (symbol);
+}
+
+/**
+ * free_symbol - free symbol
+ * @symbol: the symbol to free
+ *
+ * Return: void
+ */
+void free_symbol(symbol_t *symbol)
+{
+	if (symbol != NULL)
+	{
+		free(symbol->value);
+		free(symbol);
+	}
+}
