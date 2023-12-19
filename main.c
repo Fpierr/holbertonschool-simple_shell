787b6f9ad86263593d6ac23f9246ea03161627b7
fatima9821 <bahfatoumat9821@gmail.com>
2023-12-19 03:34:06 -0800

    simpe shell project

--- main.c
+++ main.c
@@ -1,43 +1,5 @@
 #include "shell.h"
 
-<<<<<<< HEAD
-/**
- * main - entry point for the shell program
- *
- * display_prompt - displays the shell prompt
- * display_result - displays the result of a command execution
- * @result: the result code of the command execution
- * display_error - displays an error massage
- * @message: the error message to be displayed
- * display_command - displays information about a command
- * @cmd: a pointer to the structure representing the command
- * Return: always 0
- */
-
-int main(void)
-{
-	display_prompt();
-
-	command_t *cmd;
-
-	cmd.args = (char *[]){"ls", "-l", NULL};
-	cmd.input_redirect = NULL;
-	cmd.output_redirect = NULL;
-	cmd.background = 0;
-
-	display_command(&cmd);
-
-	int result = 0;
-
-	display_result(result);
-
-	const char *errorMessage = "an error occurred";
-
-	display_error(errorMessage);
-
-	return (0);
-}
-=======
 int main(void)
 {
 	char input[INPUT_SIZE_MAX];
@@ -100,5 +62,3 @@ int main(void)
 	}
 	return (0);
 }
-
->>>>>>> 93b89c7c754b62834a7a99a87800e3b8840a0ee4
